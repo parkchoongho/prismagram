84ba590165c91dfec7bf00820b3e9e4a3886b1ef
unknown <pchyo92@gmail.com>
2020-01-13 01:53:55 +0900

    set up graphql

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger("dev"));
+
+server.start({ port: PORT }, () =>
+  console.log(`✅  Server Running on Port: http://localhost:${PORT}`)
+);
